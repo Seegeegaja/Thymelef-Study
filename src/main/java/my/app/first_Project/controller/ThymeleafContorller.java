77a34421ff8bdb04abadbf429095a7dbe7c0c466
seegeegaja <wnsdyd821@gmail.com>
2025-02-06 14:38:29 +0900

    모델 자료구조 처리

--- src/main/java/my/app/first_Project/controller/ThymeleafContorller.java
+++ src/main/java/my/app/first_Project/controller/ThymeleafContorller.java
@@ -1,10 +1,17 @@
 package my.app.first_Project.controller;
 
+import my.app.first_Project.dto.Animal;
+import my.app.first_Project.dto.Fruits;
 import my.app.first_Project.dto.UserData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/basic")
@@ -16,13 +23,41 @@ public class ThymeleafContorller {
         model.addAttribute("See", "mylove");
         return "/basic/text-basic";
     }
-    @GetMapping("/Variables")
+    @GetMapping("/variables")
     public String variables(Model model){
-        UserData userData = new UserData("홍길동", 10);
+        UserData userData = new UserData();
+        userData.setUserName("안유진");
+        userData.setUserAge(20);
+        userData.setUserAddress("서울");
         System.out.println(userData);
-        model.addAttribute("usetData", userData);
+        model.addAttribute("userData", userData);
         model.addAttribute("data", "장원영");
-        return "/basic/variables";
+
+
+        //리스트 만들기
+        List<Fruits> fruits = new ArrayList<>();
+        Fruits apple = new Fruits("사과", "신맛");
+        Fruits mango = new Fruits("망고", "노란맛");
+
+        fruits.add(apple);
+        fruits.add(mango);
+        model.addAttribute("fruits",fruits);
+        System.out.println(fruits);
+        //맵으로 작업
+        Map<String, Animal> animalMap = new HashMap<>();
+        Animal dog = new Animal();
+        dog.setName("망망이");
+        dog.setSound("망망");
+
+        Animal cat = new Animal();
+        cat.setName("나비");
+        cat.setSound("야옹");
+
+        animalMap.put("dog",dog);
+        animalMap.put("cat",cat);
+        model.addAttribute("map",animalMap);
+        return "basic/variables";
+
     }
 
 }
--- src/main/java/my/app/first_Project/dto/UserData.java
+++ src/main/java/my/app/first_Project/dto/UserData.java
@@ -1,13 +1,20 @@
 package my.app.first_Project.dto;
 
-
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Getter
+@Setter
+@ToString
 public class UserData {
     private String userName;
     private int userAge;
     private String userAddress;
 
-    public UserData(String userName, int userAge) {
-        this.userName = userName;
-        this.userAge = userAge;
-    }
+//    public UserData(String userName, int userAge) {
+//        this.userName = userName;
+//        this.userAge = userAge;
+//    }
 }
